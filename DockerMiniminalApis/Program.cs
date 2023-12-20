f488482094b1a7090b090f1177fcf6e9307de868
wenceslao reyes <wenceslao.reyes@msw.com.mx>
2023-12-20 13:02:28 -0600

    Agregar archivos de proyecto.

---
+++ DockerMiniminalApis/Program.cs
@@ -0,0 +1,76 @@
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/products", async (IConfiguration configuration) =>
+{
+    try
+    {
+        using var connection = new SqlConnection(configuration.GetConnectionString("ProductsConnectionString"));
+
+        var sql =
+        @"
+        SELECT
+            Description
+        FROM
+            Products
+        ";
+
+        var products = await connection.QueryAsync<Product>(sql);
+
+        return products;
+    }
+    catch(Exception ex)
+    {
+        throw;
+    }
+}).WithName("GetProducts")
+.WithDescription("Get a list of products' descriptions.")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
+internal record Product(string Description);
